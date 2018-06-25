45275427ebf2349cdd1ea36308e2d5b551c1f4bf
ipekhkacar <ipekhkacar@gmail.com>
2018-06-25 15:47:40 +0300

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
+gem 'jquery-rails'
 gem 'bcrypt',         '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
 # Use postgresql as the database for Active Record
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,11 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
   include ApplicationHelper
   
 end
